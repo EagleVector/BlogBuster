600c7e7f4338eac7077af9c1627ec65650a09f33
EagleVector <kumarsubhambili3@gmail.com>
2023-07-14 12:00:02 +0530

    rendering blog card

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+
+const Blog = require('./models/blog');
 
 const userRoute = require('./routes/user');
 const blogRoute = require('./routes/blog');
@@ -19,10 +21,13 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')))
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
   res.render("home", {
     user: req.user,
+    blogs: allBlogs
   });
 });
 
